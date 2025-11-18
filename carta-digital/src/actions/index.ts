9b8e03d89eecfba80423db837174f36492d462ec
Agustin Lorenzatto <agustinlorenzatto@outlook.com>
2025-11-18 14:54:18 -0300

    Documentado el codigo

--- carta-digital/src/actions/index.ts
+++ carta-digital/src/actions/index.ts
@@ -1,3 +1,13 @@
+/*
+    Esta carpeta tiene las funciones de conexion a la base de datos con cada tabla que pertence
+    es decir, cada archivo es una tabla y tiene los principales metodos CRUD ademas de alguna funcion especifica.
+
+    Estas funciones son utilizadas por los arhcivos de la carpeta hooks
+
+    En el archvio egreso.actions.ts esta documentado el codigo, los demas son parecidos
+*/
+
+
 export * from './categorias.actions';
 export * from './egreso.actions';
 export * from './pedidos.actions';
--- carta-digital/src/app/[rotiseria]/(public)/page.tsx
+++ carta-digital/src/app/[rotiseria]/(public)/page.tsx
@@ -1,38 +1,12 @@
-'use client'
-import React, { useEffect, useState } from 'react'
 
 
-import { useCarritoStore } from '@/store/useCarritoStore';
-import { useRotiseriaStore } from '@/store/useRotiseriaStore';
-import Loading from '@/components/ui/Loading';
-import { Categorias, Header, ModalCarrito, ModalClienteCarrito, Productos } from '@/components';
 
 const RotiseriaPage = () => {
-  const { modalAbierto, modalClienteCarrito } = useCarritoStore();
-  const { rotiseriaActive } = useRotiseriaStore();
-
-  const [buscador, setBuscador] = useState<string>('');
-
-  if (!rotiseriaActive) {
-    return <Loading texto='Cargando...' />
-  };
 
   return (
     <div className='h-screen bg-slate-200 text-black'>
 
-      <Header rotiseria={rotiseriaActive} />
-
-      <div>
-        <input onChange={e => setBuscador(e.target.value.toUpperCase())} type="text" placeholder='Buscar producto por nombre' className='bg-white w-full py-2 font-lg border border-gray-300 rounded-lg mx-10 mt-2 px-2' />
-      </div>
-
-      <Categorias rotiseria={rotiseriaActive} />
-
-      <Productos buscador={buscador} rotiseria={rotiseriaActive} />
-
-
-      {modalAbierto && <ModalCarrito />}
-      {modalClienteCarrito && <ModalClienteCarrito />}
+      <h1>Pagina Web</h1>
 
     </div>
   )
