815c1b47b6be1ba5f8524fc05934ddc87595b3f7
Agustin Lorenzatto <agustinlorenzatto@outlook.com>
2025-12-12 09:23:21 -0300

    Agregando otro texto

--- carta-digital/src/app/page.tsx
+++ carta-digital/src/app/page.tsx
@@ -4,7 +4,7 @@ import { headers } from 'next/headers';
 
 const Home = async () => {
   const host = (await headers()).get('host') || '';
-  console.log(host);
+  const subdomain = host.split('.')[0].split(':')[0];
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
@@ -12,7 +12,7 @@ const Home = async () => {
         <h1 className="text-4xl font-extrabold text-blue-700 mb-4">Bienvenido a la página de rotiserías</h1>
         <p className="text-lg text-gray-700 leading-relaxed">Descubre las mejores opciones de comida casera cerca de ti. Explora, elige y disfruta de sabores únicos.</p>
 
-        <Button>{host}</Button>
+        <Button>{subdomain}</Button>
       </div>
     </div>
   );
