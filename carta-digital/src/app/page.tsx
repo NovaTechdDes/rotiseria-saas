5569152ff8abff6c3d537a8dde35e5845ecb2d5f
Agustin Lorenzatto <agustinlorenzatto@outlook.com>
2025-12-13 11:25:51 -0300

    Mostrando Info de la app

--- carta-digital/src/app/page.tsx
+++ carta-digital/src/app/page.tsx
@@ -1,36 +1,40 @@
 import { Button } from '@/components/ui/Button';
 import '../styles/global.css';
-import { headers } from 'next/headers';
+import Caracteristicas from '@/components/layout/Caracteristicas';
+import { ArrowRightIcon, Info, Link } from 'lucide-react';
+import { Precio } from '@/components/layout/Precio';
+import Footer from '@/components/layout/Footer';
 
 const Home = async () => {
-  const host = (await headers()).get('host') || '';
-  const subdomain = host.split('.')[0].split(':')[0];
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 text-center">
-        <div>
-          <h1 className="mt-6 text-3xl font-extrabold text-gray-900">Software de Gestión para Rotiserías</h1>
-          <p className="mt-2 text-sm text-gray-600">Optimiza tu negocio de rotisería con nuestra solución integral.</p>
-        </div>
-        <div className="mt-8 space-y-6">
-          <p className="text-gray-700">
-            Desde el control de pedidos hasta la gestión de inventario y clientes, nuestro SaaS está diseñado para simplificar tus operaciones y aumentar tu rentabilidad.
+    <div className="min-h-screen bg-gradient-to-br from-indigo-50 to-purple-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className=" w-full rounded-xl bg-gray-200 shadow-2xl p-8 md:p-12 space-y-10 text-center transform transition-all duration-500 hover:scale-[1.01]">
+        <div id="demo" className="text-black">
+          <h1 className="text-6xl font-bold">
+            Gestiona tu rotiseria de forma <span className="text-orange-500">profesional</span>
+          </h1>
+          <p className="text-slate-500 text-lg mt-2">
+            La plataforma todo en uno para administrar productos, pedidos, egresos y reportes de tu rotisería. Simple, potente y diseñada específicamente para tu negocio.
           </p>
-          <ul className="list-disc list-inside text-left text-gray-700 mx-auto max-w-xs">
-            <li>Gestión de pedidos eficiente</li>
-            <li>Control de stock en tiempo real</li>
-            <li>Fidelización de clientes</li>
-            <li>Reportes y análisis avanzados</li>
-          </ul>
+
+          <div className="flex mt-5 justify-center w-full">
+            <Button type="button">
+              <Link href={`https://buenacomida.rotiseriasaas.com.ar/`} target="_blank" className="flex items-center">
+                Ver Demo en vivo
+                <ArrowRightIcon className="w-6 h-6 ml-2" />
+              </Link>
+            </Button>
+          </div>
+          <p className="text-sm  mt-2 text-slate-400">Sin Tarjeta de credito . Configuracion en minutos</p>
         </div>
-        <div>
-          <Button
-            type="button"
-            className="w-full justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Saber Más
-          </Button>
-        </div>
+
+        <Caracteristicas />
+
+        <Info />
+
+        <Precio />
+
+        <Footer />
       </div>
     </div>
   );
